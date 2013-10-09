be5d67bc42f9a6eea060f0aba707fe26fce4e3eb
Elad Meidar <elad@eizesus.com>
2013-10-09 15:07:13 +0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BubbleTalk::Application.config.secret_key_base = '73cb13ef5e440d751450012abf83c65533cef5b8cf4afa70d036b4d682425b009db367e0b6c2e85cfc302ec47625833e17458da2b8168047527655ed3e02d52a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BubbleTalk::Application.config.session_store :cookie_store, key: '_bubble_talk_session'
